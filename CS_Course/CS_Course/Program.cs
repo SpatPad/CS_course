7145d1b03f06bcf02d27a6b5b00053c46eaaf78f
SpatPad <wirga.krzysztof@gmail.com>
2022-05-08 18:04:02 +0200

    If/else

--- CS_Course/CS_Course/Program.cs
+++ CS_Course/CS_Course/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading.Channels;
 
 namespace FirstProject
 {
@@ -7,94 +8,25 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            int result;
-            int x = 10;
-            int y = 5;
+            Console.WriteLine("Year of birth?");
+            string userInput = Console.ReadLine();
 
-            result = (x + y);
-            Console.WriteLine("Addition Operator: x + y = " + result);
+            int yearOfBirth = int.Parse(userInput);
+            bool isUserOver18 = DateTime.Now.Date.Year - yearOfBirth > 18;
+            if (isUserOver18)
+            {
+                Console.WriteLine("hello");
+            }
+            else if(DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                Console.WriteLine("Oh well its Sunday");
+            }
+            else
+            {
+                Console.WriteLine("Access denied");
+            }
 
-            result = (x - y);
-            Console.WriteLine("Subtraction Operator: x - y = " + result);
-
-            result = (x * y);
-            Console.WriteLine("Multiplication Operator: x * y = " + result);
-
-            result = (x / y);
-            Console.WriteLine("Division Operator: x / y = " + result);
-
-            result = (x % y);
-            Console.WriteLine("Modulo Operator: x % y = " + result);
-
-            int value = (2 + 2) * 5; 
-
-
-            Console.WriteLine("************");
-
-
-            int value1 = 10;
-            int value2 = 10;
-
-            Console.WriteLine("Pre-incremented result:");
-            Console.WriteLine(++value1);
-
-            Console.WriteLine("Post-incremented result:");
-            Console.WriteLine(value2++);
-            Console.WriteLine(value2);
-
-
-            Console.WriteLine("************");
-
-
-            bool boolResult;
-            x = 2;
-            y = 10;
-
-            boolResult = (x == y);
-            Console.WriteLine("Equal to Operator: (x == y) = " + boolResult);
-
-            boolResult = (x > y);
-            Console.WriteLine("Greater than Operator: (x > y) = " + boolResult);
-
-            boolResult = (x < y);
-            Console.WriteLine("Less than Operator: (x < y) = " + boolResult);
-
-            boolResult = (x >= y);
-            Console.WriteLine("Greater than or equal to: (x >= y) = " + boolResult);
-
-            boolResult = (x <= y);
-            Console.WriteLine("Lesset than or Equal to: (x <= y) = " + boolResult);
-
-            boolResult = (x != y);
-            Console.WriteLine("Not Equal to Operator: (x != y) = " + boolResult);
-
-
-            Console.WriteLine("************");
-
-
-            bool a = true;
-            bool b = false;
-
-            boolResult = a && b;
-            Console.WriteLine("AND Operator: a && b = " + boolResult);
-
-            boolResult = a || b;
-            Console.WriteLine("OR Operator: a || b = " + boolResult);
-
-            boolResult = !a;
-            Console.WriteLine("NOT Operator: !a = " + boolResult);
-
-
-            Console.WriteLine("************");
-
-
-            x = 5;
-            y = 10;
-
-            int greaterNumber = x > y ? x : y;
-            Console.WriteLine("Tenary conditional operator: x > y ? x : y = " + greaterNumber);
-
-
+            Console.WriteLine("bye");
         }
     }
 }
