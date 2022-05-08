4f47aa92c8e3d600c5032818e33c5c787f2ffad5
SpatPad <wirga.krzysztof@gmail.com>
2022-05-08 12:47:52 +0200

    Project created

---
+++ CS_Course/CS_Course/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FirstProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string message1;
+            string message2 = null;
+
+            string message3 = string.Empty;
+            string text = "He said \"Hi\"";
+
+            string windowLocation = "c:\\windows";
+
+            string fontsFolder = @"c:\windows\fonts";
+
+            string concatenated = string.Concat(text, "");
+
+
+
+
+
+
+        }
+    }
+}
